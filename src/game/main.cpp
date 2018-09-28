9428a5c700598313d2d76cb399893928a69d73e8
HiddenRealm <jamestaylor034@gmail.com>
2018-09-28 13:02:02 +0100

    This is with the small changes the change hello world to be the beginnings of the engine

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,8 +1,21 @@
-#include<iostream>
+#include<myengine/myengine.h>
+#include<memory>
+
+using namespace myengine;
 
 int main()
 {
-std::cout << "Hello World" << std::endl;
+	//Init Engine
+	std::shared_ptr<Core> core = Core::initialize();
+
+	//Single Item
+	std::shared_ptr<Entity> entity = core->addEntity();
+
+	//Simple Component
+	std::shared_ptr<TestScreen> testscreen = entity->addComponent<TestScreen>();
+
+	//Start Loop
+	core->start();
 
 return 0;
 }
