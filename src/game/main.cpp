bbdb5e079441ff5dc45dde796bcdce6bdf3c66f9
HiddenRealm <jamestaylor034@gmail.com>
2018-10-05 13:34:55 +0100

    This update adds a bunch of dependences and the code they need to work, i now need to add the code to project a triangle to the screen.

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -1,21 +1,8 @@
-#include<myengine/myengine.h>
-#include<memory>
-
-using namespace myengine;
+#include <iostream>
 
 int main()
 {
-	//Init Engine
-	std::shared_ptr<Core> core = Core::initialize();
+	std::cout << "Hello World" << std::endl;
 
-	//Single Item
-	std::shared_ptr<Entity> entity = core->addEntity();
-
-	//Simple Component
-	std::shared_ptr<TestScreen> testscreen = entity->addComponent<TestScreen>();
-
-	//Start Loop
-	core->start();
-
-return 0;
+	return 0;
 }
---
+++ src/myengine/Core.h
@@ -0,0 +1,18 @@
+Class Core noncopyable //Check this
+{
+Public:
+
+
+void Core::initialize(); */
+void Core::~Core();
+void Core::start();      all of these are wrong i think
+void Core::stop();
+void Core::addEntity()  /*
+
+
+Private:
+
+
+
+
+}
